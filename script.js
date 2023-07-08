7ec31cd9c70441c3c641274325fb5901459db745
althafmkallus <135153557+althafmkallus@users.noreply.github.com>
2023-07-08 20:15:29 +0530

    Add files via upload
    f

---
+++ script.js
@@ -0,0 +1,154 @@
+// Animation on page load
+window.addEventListener('load', function() {
+  // Animate the header and sections
+  // ...
+
+  // Animate the cards on touch/click
+  var cards = document.querySelectorAll('.card');
+  if (cards) {
+    cards.forEach(function(card) {
+      card.addEventListener('click', function() {
+        animateCard(this);
+      });
+    });
+  }
+
+  function animateCard(card) {
+    card.style.transition = 'transform 0.3s ease';
+    card.style.transform = 'scale(0.95)';
+    setTimeout(function() {
+      card.style.transform = 'scale(1)';
+    }, 300);
+  }
+});
+
+// Add smooth scrolling
+// ...
+// Add JavaScript code to handle the slideshow effect
+window.addEventListener('load', function() {
+  var slides = document.querySelectorAll('.slideshow img');
+  var currentSlide = 0;
+  var slideInterval = setInterval(nextSlide, 2000); // Change slide every 2 seconds
+
+  slides[currentSlide].classList.add('active');
+
+  function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+  }
+});
+
+
+  particlesJS("background-animation", {
+    // Configure the particle animation here
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#ffffff",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100,
+        },
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: true,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1,
+          },
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
+  });
+
